b0cb8a0a7b7bc5d1085acd8c774230a0c3c54c7b
Cristiano Longo <cristianolongo@gmail.com>
2016-06-28 23:02:18 +0200

    #38 add access counter

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * store on a file access.log date and time of accessing this code.
+ */
+define('CSV_TIME_FORMAT',"Y\tm\td\tH\ti\ts\n");
+$handle = fopen("access.log", "a");
+fwrite($handle, date(CSV_TIME_FORMAT));
+fflush($handle);
+fclose($handle);
+?>
 <!doctype html>
 <html>
 <head>
