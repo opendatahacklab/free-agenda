309d67a859865dac2d059e740b09abba9c8e43d2
cristianolongo <cristianolongo@gmail.com>
2016-12-01 11:24:20 +0100

    minor changes

--- agenda-unica-ct/constants.php
+++ agenda-unica-ct/constants.php
@@ -1,6 +1,7 @@
 <?php 
 define('ONTOLOGY', 'http://opendatahacklab.org/free-agenda/agenda-unica-ct/ontology');
 define('SHEET','https://docs.google.com/spreadsheets/d/1bzVASM5_JjCgvNp3Vs0GJ4vDgYsKo_ig5NHU1QI5USc/export?format=tsv&exportFormat=tsv&ndplr=1');
-define('SPARQL_ENDPOINT','http://dydra.com/cristianolongo/agenda-unica-ct/sparql');
+//define('SPARQL_ENDPOINT','http://dydra.com/cristianolongo/agenda-unica-ct/sparql');
+define('SPARQL_ENDPOINT','http://localhost:9001/execQuery');
 define('TITLE','Agenda Unica del Movimento - Catania');
 ?>
---
+++ agenda-unica-ct/semanticoctopus.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+java -jar ../../semanticoctopus/target/semanticoctopus-0.1.1.jar http://localhost/~cristianolongo/opendatahacklab/free-agenda/agenda-unica-ct/ontology.php
+
+
