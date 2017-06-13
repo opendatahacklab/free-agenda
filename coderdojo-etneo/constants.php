3b27d6a8d329c3cee6942ca913a42e6dcd917b85
cristianolongo <cristianolongo@gmail.com>
2017-06-13 11:59:55 +0200

    ontology backup from sparql dump

--- coderdojo-etneo/constants.php
+++ coderdojo-etneo/constants.php
@@ -1,6 +1,6 @@
 <?php 
-define('ONTOLOGY', 'http://opendatahacklab.org/free-agenda/coderdojo-etneo/ontology.php');
+define('ONTOLOGY', 'http://opendatahacklab.org/free-agenda/coderdojo-etneo/ontologybak.php');
 define('SHEET','https://docs.google.com/spreadsheets/d/1LgQJRHa_m3MH7Tyoeg5kOsy89QluTG4YKBOkox372Uc/export?format=tsv&exportFormat=tsv&ndplr=1');
-define('SPARQL_ENDPOINT','http://dydra.com/cristianolongo/coderdojo-etneo/sparql');
+define('SPARQL_ENDPOINT','http://dydra.com/cristianolongo/coderdojo-etnet/sparql');
 define('TITLE','CoderDojo Etneo');
 ?>
